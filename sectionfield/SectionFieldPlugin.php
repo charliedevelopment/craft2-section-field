a1da2c448a4642d64e565c60d0cce3d75dfe3d43
Webmaster <chris.saign@frankandvictor.com>
2017-12-05 17:45:40 -0600

    1.0.1
    Updated version
    Added documentation link
    Release feed updated

--- sectionfield/SectionFieldPlugin.php
+++ sectionfield/SectionFieldPlugin.php
@@ -5,6 +5,11 @@ namespace Craft;
 class SectionFieldPlugin extends BasePlugin {
 
 	public function getVersion() {
+		return '1.0.1';
+	}
+	
+	public function getSchemaVersion()
+	{
 		return '1.0.0';
 	}
 
@@ -24,8 +29,13 @@ class SectionFieldPlugin extends BasePlugin {
 		return 'http://charliedev.com/';
 	}
 	
+	public function getDocumentationUrl()
+	{
+		return 'https://github.com/charliedevelopment/Craft2-Section-Field/blob/master/README.md';
+	}
+	
 	public function getReleaseFeedUrl()
 	{
-		return 'https://raw.githubusercontent.com/charliedevelopment/Section-Field/master/release.json'
+		return 'https://raw.githubusercontent.com/charliedevelopment/Craft2-Section-Field/master/release.json';
 	}	
 }
